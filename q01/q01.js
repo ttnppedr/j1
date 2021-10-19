b6ffdf8e08b5882228895c669302e15149a76ff4
Rex <smilerex@gmail.com>
2021-10-19 16:04:26 +0800

    first commit

---
+++ q01/q01.js
@@ -0,0 +1,8 @@
+// var guest = 0;
+// var lowerLimit = 0;
+
+// if (guest !== lowerLimit) {
+//   console.log("顧客人數 " + guest + " 人, 照常播放電影.");
+// } else {
+//   console.log("顧客人數 " + guest + " 人, 不播放電影.");
+// }
