4ae21cc3e2acf11a5f5d51cb5c1d7c2ba2c55282
Rex <smilerex@gmail.com>
2021-11-09 17:19:48 +0800

    Revert "first commit"
    This reverts commit b6ffdf8e08b5882228895c669302e15149a76ff4.

--- q01/q01.js
+++
@@ -1,8 +0,0 @@
-// var guest = 0;
-// var lowerLimit = 0;
-
-// if (guest !== lowerLimit) {
-//   console.log("顧客人數 " + guest + " 人, 照常播放電影.");
-// } else {
-//   console.log("顧客人數 " + guest + " 人, 不播放電影.");
-// }
