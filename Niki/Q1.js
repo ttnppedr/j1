d7e51deac7f16d3a8b02544c645edf2dc5f7ffb2
Niki wei <frank29580649@gmail.com>
2022-11-03 07:27:53 +0800

    jest test

--- Niki/Q1.js
+++ Niki/Q1.js
@@ -35,3 +35,5 @@ function check(input) {
 function result(input) {
   return input === 0 ? "今天不播放電影" : "照常播放電影";
 }
+
+module.exports = result
--- Niki/Q1.test.js
+++ Niki/Q1.test.js
@@ -1,17 +1,17 @@
-const movie = require("./Q1");
+const (check,result) = require("./Q1");
 
 test("0  to 今天不播放電影", () => {
-  expect(movie(0)).toEqual("今天不播放電影");
+  expect(result(0)).toEqual("今天不播放電影");
 });
 test("1  to 今天不播放電影", () => {
-  expect(movie(1)).toEqual("照常播放電影");
+  expect(result(1)).toEqual("照常播放電影");
 });
-test("1  to 今天不播放電影", () => {
-  expect(2+2).toBe(4);
-});
-test("1  to 今天不播放電影", () => {
-  expect(2+2).toEqual(4);
-});
-test("1  to 今天不播放電影", () => {
-  expect(2+2).not.toEqual(5);
-});
+// test("1  to 今天不播放電影", () => {
+//   expect(2+2).toBe(4);
+// });
+// test("1  to 今天不播放電影", () => {
+//   expect(2+2).toEqual(4);
+// });
+// test("1  to 今天不播放電影", () => {
+//   expect(2+2).not.toEqual(5);
+// });
