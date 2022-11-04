2ac82b2da99d5325bc2ae0126410aa2c1b09638a
Niki wei <frank29580649@gmail.com>
2022-11-04 08:44:21 +0800

    Q1 module test

--- Niki/Q1/Q1.js
+++ Niki/Q1/Q1.js
@@ -1,18 +1,17 @@
 // 1. 好想電影院規定除非完全沒有客人買票，否則就算只有一位顧客也照常播放電影。讓使用者輸入客人的人數，並用「!」判斷人數，不為零顯示「照常播放電影」。
 
-let openReadLine = require("../readline/openReadLine"); //開啟readline
-let checkEmpty = require("../ckeck/checkEmpty"); //空鑑定
-let checkString = require("../ckeck/checkString"); //字串鑑定
-let checkDecimal = require("../ckeck/checkDecimal"); // 十進位鑑定
-let checkPositive = require("../ckeck/checkPositive"); //正數鑑定
-let checkInteger = require("../ckeck/checkInteger"); //整數鑑定
-// let result = require("./Q1.module") 
-import {result} from  "Q1.module"
+import { openReadLine } from "../readline-ES6/openReadLine.js"; //開啟readline
+import { checkEmpty } from "../check-ES6/checkEmpty.js"; //空鑑定
+import { checkString } from "../check-ES6/checkString.js"; //字串鑑定
+import { checkDecimal } from "../check-ES6/checkDecimal.js"; // 十進位鑑定
+import { checkPositive } from "../check-ES6/checkPositive.js"; //正數鑑定
+import { checkInteger } from "../check-ES6/checkInteger.js"; //整數鑑定
+import { result } from "./Q1.module.js";
 
 main("今天好想電影院有幾人買票??");
 
 async function main(question) {
-  input = await openReadLine(question);
+  let input = await openReadLine(question);
   if (check(input)) {
     console.log(result(Number(input)));
   } else {
--- Niki/Q1/Q1.test.js
+++ Niki/Q1/Q1.test.js
@@ -1,9 +1,11 @@
-const (check,result) = require("./Q1");
+// const result = require("./Q1.module");
+
+import {result} from "./Q1.module.js"
 
 test("0  to 今天不播放電影", () => {
   expect(result(0)).toEqual("今天不播放電影");
 });
-test("1  to 今天不播放電影", () => {
+test("1  to 照常播放電影", () => {
   expect(result(1)).toEqual("照常播放電影");
 });
 // test("1  to 今天不播放電影", () => {
