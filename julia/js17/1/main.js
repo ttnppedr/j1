bd5abcce12b8c222ee6a29b1b977ecf15fd21b44
ttnppedr <ttnppedr@fastmail.com>
2023-11-13 11:44:49 +0800

    Fix question 1 code style

--- julia/js17/1/main.js
+++ julia/js17/1/main.js
@@ -1,20 +1,20 @@
 // Q01. 好想電影院規定除非完全沒有客人買票，否則就算只有一位顧客也照常播放電影。
 // 讓使用者輸入客人的人數，並用「!」判斷人數，不為零顯示「照常播放電影」。
 
-import prompt from "../utils/getUserInput.js";
-import { playMovie } from "./playMovie.js";
-
+import prompt from "../utils/getUserInput.js"
+import { playMovie } from "./playMovie.js"
 
 main()
 
 function main() {
   const input = prompt('請問總共幾個人看電影？')
+
   try {
     const result = playMovie(input)
-    console.log(result);
-
+    console.log(result)
   } catch (error) {
-    console.log(error.message);
+    console.log(error.message)
+
     return main()
   }
 }
--- julia/js17/1/playMovie.test.js
+++ julia/js17/1/playMovie.test.js
@@ -1,26 +1,30 @@
-import { playMovie } from "./playMovie.js";
+import { playMovie } from "./playMovie.js"
 
 describe('檢查到底要不要播電影', () => {
   test('人數0', () => {
     const input = "0"
     const result = playMovie(input)
+
     expect(result).toBe(`停播`)
   })
 
   test('只有1人', () => {
     const input = "1"
     const result = playMovie(input)
+
     expect(result).toBe(`照常播電影`)
   })
 
   test('1000人', () => {
     const input = "1000"
     const result = playMovie(input)
+
     expect(result).toBe(`照常播電影`)
   })
 
   test('輸入含空白格', () => {
     const input = " "
+
     expect(() => {
       playMovie(input)
     }).toThrow("不能是空白字元喔！")
@@ -28,6 +32,7 @@ describe('檢查到底要不要播電影', () => {
 
   test('非整數', () => {
     const input = "-0.5"
+
     expect(() => {
       playMovie(input)
     }).toThrow("請輸入整數阿拉伯數字喔！")
@@ -35,13 +40,15 @@ describe('檢查到底要不要播電影', () => {
 
   test('負數', () => {
     const input = "-1"
+
     expect(() => {
       playMovie(input)
     }).toThrow("不能小於0啦")
   })
 
-    test('16進制', () => {
+  test('16進制', () => {
     const input = "0x123"
+
     expect(() => {
       playMovie(input)
     }).toThrow("不支援16進制,請輸入0~9阿拉伯數字")
@@ -49,10 +56,9 @@ describe('檢查到底要不要播電影', () => {
 
   test('e科學符號', () => {
     const input = "1e"
+
     expect(() => {
       playMovie(input)
     }).toThrow("不支援e科學符號,請輸入0~9阿拉伯數字")
   })
-
-
-}) 
+})
